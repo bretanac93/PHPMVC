ae8a952576c7103756bef40d9c334bdc6b93a66e
Cesar Bretana Gonzalez <cesar.bretana@estudiantes.upr.edu.cu>
2014-09-05 19:20:27 +0200

    Updated

--- libs/Controller.php
+++ libs/Controller.php
@@ -7,4 +7,14 @@ class Controller {
     public function render($name){
         require 'View/'.$name.'.php';
     }
+    
+    public function loadModel($name){
+        $path = 'Model/'.$name.'_model.php';
+        
+        if(file_exists($path)){
+            require 'Model/'.$name.'_model.php';
+            $modelName = $name.'_model';
+            $this->model = new $modelName();
+        }
+    }
 }
