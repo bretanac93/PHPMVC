f53887947be03f82cd31908bf0b6a9bd438aa832
Cesar Bretana Gonzalez <cesar.bretana@estudiantes.upr.edu.cu>
2014-09-05 19:21:43 +0200

    Updated

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require 'libs/Controller.php';
 require 'libs/View.php';
 require 'libs/Model.php';
 
-require 'libs/Database';
+require 'libs/Database.php';
 require 'config/paths.php';
 require 'config/database.php';
 
