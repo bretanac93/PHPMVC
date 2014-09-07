f132be8eab0b763d94686b4e820e2a926ffba5f2
Cesar Bretana Gonzalez <cesar.bretana@estudiantes.upr.edu.cu>
2014-09-07 09:34:14 +0200

    Connection to the database added

--- Model/login_model.php
+++ Model/login_model.php
@@ -5,7 +5,23 @@ class Login_Model extends Model{
     function __construct() {
         parent::__construct();
     }
-    public function test(){
-        
+    public function run(){
+        $sth = $this->db->prepare("SELECT id FROM users WHERE 
+                login = :login AND password = MD5(:password)");
+        $sth->execute(array(
+            ':login' => $_POST['login'],
+            ':password' => $_POST['password']
+        ));
+        //$data = $sth->fetchAll();
+        $count = $sth->rowCount();
+        if($count > 0){
+            //login
+            Session::init();
+            Session::set('loggedIn', true);
+            header('location: ../dashboard');
+        }
+        else{
+            header('location: ../login');
+        }
     }
 }
