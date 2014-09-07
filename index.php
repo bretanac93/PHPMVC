9ff96136317fc3a999a1c1c056ecc0d0f9f3c3ef
Cesar Bretana Gonzalez <cesar.bretana@estudiantes.upr.edu.cu>
2014-09-07 09:44:19 +0200

    Already included new configurations and libs

--- index.php
+++ index.php
@@ -4,8 +4,9 @@ require 'libs/Bootstrap.php';
 require 'libs/Controller.php';
 require 'libs/View.php';
 require 'libs/Model.php';
-
 require 'libs/Database.php';
+require 'libs/Session.php';
+
 require 'config/paths.php';
 require 'config/database.php';
 
