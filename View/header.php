4d4a5beb79a0e18771062a9fbf37a3d1e0b0c6e1
Cesar Bretana Gonzalez <cesar.bretana@estudiantes.upr.edu.cu>
2014-09-07 09:37:00 +0200

    Change between 'login' and 'logout' labels depending of the session status

--- View/header.php
+++ View/header.php
@@ -7,13 +7,18 @@
         <script src="<?php echo URL;?>public/js/custom.js"></script>
     </head>
     <body>
+        <?php Session::init(); ?>
         <header>
             Header
             <br />
             <ul>
                 <li><a href="<?php echo URL;?>index">Index</a></li>
                 <li><a href="<?php echo URL;?>Help">About</a></li>
-                <li><a href="<?php echo URL;?>login">Login</a></li>
+                <?php if(Session::get('loggedIn') == true): ?>
+                    <li><a href="<?php echo URL;?>dashboard/logout">Logout</a></li>
+                <?php else: ?>
+                    <li><a href="<?php echo URL;?>login">Login</a></li>
+                <?php endif; ?>
             </ul>
         </header>
         <div id="content">
