dd7ea1c6eb0ac5f03f309e20545dd26c9a9c5800
Cesar Bretana Gonzalez <cesar.bretana@estudiantes.upr.edu.cu>
2014-09-07 09:39:52 +0200

    Added the login form

--- View/login/index.php
+++ View/login/index.php
@@ -1,1 +1,6 @@
-This is the login page!
+<h1>Login</h1>
+<form action="login/run" method="POST">
+    <input type="text" name="login" placeholder="Username" /><br />
+    <input type="password" name="password" placeholder="Password" /><br />
+    <label></label><input type="submit"/>
+</form>
