74d9e9e024b35b38b6e8fe9b105998a15f7e1ceb
Cesar Bretana Gonzalez <cesar.bretana@estudiantes.upr.edu.cu>
2014-09-07 09:45:50 +0200

    Some bugs fixed.

--- libs/Bootstrap.php
+++ libs/Bootstrap.php
@@ -3,40 +3,52 @@
 class Bootstrap {
 
     function __construct() {
-        $url = isset($_GET['url']) ? $_GET['url']: null;
+        $url = isset($_GET['url']) ? $_GET['url'] : null;
         $url = rtrim($url, '/');
         $url = explode('/', $url);
 
         //print_r($url);
 
-        if(empty($url[0])){
+        if (empty($url[0])) {
             require 'Controller/index.php';
             $controller = new Index();
             $controller->index();
             return false;
         }
-        
+
         $file = 'Controller/' . $url[0] . '.php';
-        if(file_exists($file)){
+        if (file_exists($file)) {
             require $file;
-        }
-        else{
-            require 'Controller/error.php';
-            $controller = new Error();
-            return false;
+        } else {
+            $this->error();
         }
         $controller = new $url[0];
-
         $controller->loadModel($url[0]);
+
         if (isset($url[2])) {
-            $controller->{$url[1]}($url[2]);
+            if (method_exists($controller, $url[1])) {
+                $controller->{$url[1]}($url[2]);
+            } else {
+                $this->error();
+            }
         } else {
             if (isset($url[1])) {
-                $controller->{$url[1]}();
-            }  else {
+                if (method_exists($controller, $url[1])) {
+                    $controller->{$url[1]}();
+                } else {
+                    $this->error();
+                }
+            } else {
                 $controller->index();
             }
         }
+    }
+
+    function error() {
+        require 'Controller/error.php';
+        $controller = new Error();
+        $controller->index();
+        return false;
     }
 
 }
